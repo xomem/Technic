48431bbd5e2786b4aa697e37d053844d529b3ba9
xomem <35069727+xomem@users.noreply.github.com>
2018-04-01 22:04:45 +0300

    first
    not load mainMenu

---
+++ Technic/Technic/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Technic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainMenu mainMenu;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            mainMenu = new MainMenu(this);
+            MainFrame.Navigate(mainMenu);
+        }
+        public void connectError(Exception ex)
+        {
+            MessageBox.Show(ex.Message.ToString(), ex.Source.ToString(), MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
